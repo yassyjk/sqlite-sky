e1aba652c8226dc2fce206118eda158d773bf6e9
yassyjk <yassyjk@gmail.com>
2025-01-02 21:02:39 +0900

    lib.rsのpathをwindowsでも動作するようにreplace修正

--- sky/src-tauri/src/lib.rs
+++ sky/src-tauri/src/lib.rs
@@ -63,7 +63,7 @@ pub fn run() {
             let app_dir = app.path().app_data_dir().unwrap();
             let db_path = app_dir.join(database::BSKY_DB);
 
-            database::init(&db_path.to_str().unwrap()).map_err(|e| e.to_string())?;
+            database::init(&db_path.to_str().unwrap().replace("\\", "\\\\")).map_err(|e| e.to_string())?;
 
             Ok(())
         })
